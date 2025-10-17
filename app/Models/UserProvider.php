9d91010041e458b893d4ca7cc0a3714df3b21d92
白开水浇花 <fengqiyue@gmail.com>
2025-10-18 05:08:21 +0800

    feat: add UserProvider model and migration

---
+++ app/Models/UserProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserProvider extends Model
+{
+    protected $fillable = [
+        'provider_id',
+        'name',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
