b1d63c72bf2109a4b1377e835156f7c4ede5d3cc
白开水浇花 <fengqiyue@gmail.com>
2025-10-18 00:11:11 +0800

    feat: add laravel-api-response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Mitoop\Http\RespondsWithJson;
+
 abstract class Controller
 {
-    //
+    use RespondsWithJson, ValidatesRequests;
 }
